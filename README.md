029fc8fbb3ed91c0d2557c4b512a30a8c7bdcb3e
Dan Liew <daniel.liew@imperial.ac.uk>
2014-01-15 16:56:05 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+Symboolix
+=========
+
+The Symbolic execution engine for Boogie
+
+Getting started
+===============
+
+We use git-remote-hg [1] so we can treat the Mercurial Boogie repository 
+just like a Git repository. Make sure it's installed before you start.
+
+We use Git's submodule feature for the Boogie folder. To set things up
+run...
+
+```
+$ git submodule init
+$ git submodule update
+```
---
+++ symbooglix/symbooglix/driver.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft;
+
+namespace symbooglix
+{
+	public class driver
+	{
+		public static int Main(String[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			if (args.Length == 0) {
+				Console.WriteLine ("Pass boogie file as first arg!");
+				return 1;
+			}
+
+			Microsoft.Boogie.Program x = null;
+			System.Collections.Generic.List<string> defines = null;
+			int success = Parser.Parse (args[0], defines, out x);
+			return success;
+		}
+	}
+}
+
