e0fc8f3b8e341b282459b543b30716aab239dc95
Dan Liew <daniel.liew@imperial.ac.uk>
2014-01-31 15:56:49 +0000

    Fixed use of tabs in source. Let's never use them again!

--- symbooglix/symbooglix/driver.cs
+++ symbooglix/symbooglix/driver.cs
@@ -3,21 +3,21 @@ using Microsoft;
 
 namespace symbooglix
 {
-	public class driver
-	{
-		public static int Main(String[] args)
-		{
-			Console.WriteLine ("Hello World!");
-			if (args.Length == 0) {
-				Console.WriteLine ("Pass boogie file as first arg!");
-				return 1;
-			}
+    public class driver
+    {
+        public static int Main(String[] args)
+        {
+            Console.WriteLine ("Hello World!");
+            if (args.Length == 0) {
+                Console.WriteLine ("Pass boogie file as first arg!");
+                return 1;
+            }
 
-			Microsoft.Boogie.Program x = null;
-			System.Collections.Generic.List<string> defines = null;
-			int success = Parser.Parse (args[0], defines, out x);
-			return success;
-		}
-	}
+            Microsoft.Boogie.Program x = null;
+            System.Collections.Generic.List<string> defines = null;
+            int success = Parser.Parse (args[0], defines, out x);
+            return success;
+        }
+    }
 }
 
