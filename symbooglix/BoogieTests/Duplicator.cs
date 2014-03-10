0b53b70ce5a1637f46d8cd63b20832ad2849d659
Dan Liew <daniel.liew@imperial.ac.uk>
2014-03-10 16:54:40 +0000

    Added UnitTests for Boogie. These show there are bugs in Boogie's duplicator which I need to fix (Namely BvExtractExpr and BvConcatExpr)

---
+++ symbooglix/BoogieTests/Duplicator.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+using System;
+using Microsoft.Boogie;
+using Microsoft.Basetypes;
+
+namespace BoogieTests
+{
+	[TestFixture ()]
+	public class DuplicatorTests
+	{
+		Duplicator d;
+
+		[SetUp]
+		public void init()
+		{
+			d = new Duplicator();
+		}
+
+		[Test ()]
+		public void BVConcatExpr()
+		{
+			var bv1_8 = new LiteralExpr(Token.NoToken, BigNum.FromInt(1), 8);
+			var bv2_8 = new LiteralExpr(Token.NoToken, BigNum.FromInt(2), 8);
+			var A = new BvConcatExpr(Token.NoToken, bv1_8, bv2_8);
+			var B = d.Visit(A);
+
+			// The duplicator should ensure we get new BVConcatExprs
+			Assert.IsFalse(Object.ReferenceEquals(A, B));
+		}
+
+		[Test()]
+		public void BvExtractExpr()
+		{
+			var bv2_8 = new LiteralExpr(Token.NoToken, BigNum.FromInt(2), 8);
+			var A = new BvExtractExpr(Token.NoToken, bv2_8, 6,0);
+			var B = d.Visit(A);
+
+			// The duplicator should ensure we get new BVExtractExprs
+			Assert.IsFalse(Object.ReferenceEquals(A, B));
+		}
+
+		[Test()]
+		public void NaryExpr()
+		{
+			var bv1_8 = new LiteralExpr(Token.NoToken, BigNum.FromInt(1), 8);
+			var bv2_8 = new LiteralExpr(Token.NoToken, BigNum.FromInt(2), 8);
+			var A = NAryExpr.Eq (bv1_8, bv2_8);
+			var B = d.Visit(A);
+
+			Assert.IsFalse(Object.ReferenceEquals(A, B));
+		}
+	}
+}
+
