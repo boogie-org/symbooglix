224432958de8e69686456519721c8200798e24d8
Dan Liew <daniel.liew@imperial.ac.uk>
2014-06-09 23:55:53 +0100

    Provide most stub code for ExprSMTLIBPrinter and partially implement printing literals.

---
+++ symbooglix/SymbooglixLibTests/ExprSMTLIB/Literal.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+using System;
+using Microsoft.Boogie;
+using Microsoft.Basetypes;
+using System.IO;
+using symbooglix;
+
+namespace SymbooglixLibTests
+{
+    [TestFixture()]
+    public class Literal
+    {
+        public Literal()
+        {
+            SymbooglixTest.setupDebug();
+        }
+
+        [Test()]
+        public void bitvector()
+        {
+            var literal = new LiteralExpr(Token.NoToken, BigNum.FromInt(19), 32); // 19bv32
+            checkLiteral(literal, "(_ bv19 32)");
+        }
+
+        [Test()]
+        public void Bools()
+        {
+            checkLiteral(Expr.True, "true");
+            checkLiteral(Expr.False, "false");
+        }
+
+        private void checkLiteral(LiteralExpr e, string expected)
+        {
+            using (var writer = new StringWriter())
+            {
+                var printer = new ExprSMTLIBPrinter(writer);
+                printer.Traverse(e);
+                Assert.IsTrue(writer.ToString() == expected);
+            }
+        }
+    }
+}
+
---
+++ symbooglix/symbooglix/Expr/ExprSMTLIBPrinter.cs
@@ -0,0 +1,345 @@
+using System;
+using Microsoft.Boogie;
+using System.IO;
+
+namespace symbooglix
+{
+    public class ExprSMTLIBPrinter : Traverser, IExprVisitor
+    {
+        private TextWriter TW = null;
+        public ExprSMTLIBPrinter(TextWriter TW) : base(null) // HACK: Can't pass "this" here.
+        {
+            this.TW = TW;
+            this.Visitor = this;
+        }
+
+        public override Expr Traverse(Expr root)
+        {
+            // This Traversal is recursive
+            return Visit(root);
+        }
+
+        public Expr VisitLiteralExpr(LiteralExpr e)
+        {
+            if (e.isBool)
+            {
+                if (e.IsTrue)
+                    TW.Write("true");
+                else
+                    TW.Write("false");
+            }
+            else if (e.isBvConst)
+            {
+                // FIXME: Support other bit vector literal printing modes
+                TW.Write(string.Format("(_ bv{0} {1})", e.asBvConst.Value, e.asBvConst.Bits));
+            }
+            else if (e.isBigNum)
+            {
+                TW.Write(e.asBigNum);
+            }
+            else
+            {
+                throw new NotImplementedException("LiteralExpr type not handled");
+            }
+
+            return e;
+        }
+
+        public Expr VisitIdentifierExpr (IdentifierExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitOldExpr (OldExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitCodeExpr (CodeExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitBvExtractExpr (BvExtractExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitBvConcatExpr (BvConcatExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitForallExpr (ForallExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitExistExpr (ExistsExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitLambdaExpr (LambdaExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitNot (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitNeg (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitAdd (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitSub (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitMul (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitDiv (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitMod (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitRealDiv (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitEq (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitNeq (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitGt (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitGe (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitLt (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitLe (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitAnd (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitOr (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitImp (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitIff (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitSubType (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitMapStore (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitMapSelect (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitIfThenElse (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitFunctionCall (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitTypeCoercion (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr VisitArithmeticCoercion (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvadd (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvsub (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvmul (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvudiv (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvurem (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvsdiv (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvsrem (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvsmod (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_sign_extend (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_zero_extend (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvneg (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvand (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvor (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvnot (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvxor (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvshl (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvlshr (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvashr (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvult (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvule (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvugt (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvuge (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvslt (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvsle (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvsgt (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public Expr Visit_bvsge (NAryExpr e)
+        {
+            throw new NotImplementedException ();
+        }
+
+    }
+}
+
