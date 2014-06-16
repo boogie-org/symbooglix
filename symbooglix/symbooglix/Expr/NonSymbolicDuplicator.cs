1ffc5fe9fa65bade16f592137b2b776fd0d9ccb6
Dan Liew <daniel.liew@imperial.ac.uk>
2014-06-16 23:51:30 +0100

    Fix Symbolic duplication bug (I think). It seems to be a bug in Boogie that IdentifierExprs can be cloned twice
    foo(symbolic_0) == foo(symbolic_0) seemed to expose this.
    
    I should really fix Boogie at some point.

--- symbooglix/symbooglix/Expr/NonSymbolicDuplicator.cs
+++ symbooglix/symbooglix/Expr/NonSymbolicDuplicator.cs
@@ -18,10 +18,22 @@ namespace symbooglix
             if (node.Decl is SymbolicVariable)
             {
                 Debug.Assert(node == ( node.Decl as SymbolicVariable ).expr, "Mismatched Symbolic IdentifierExpr");
+                if (node != ( node.Decl as SymbolicVariable ).expr)
+                    throw new Exception("FIXME");
                 return node;
             }
             else
                 return base.VisitIdentifierExpr (node);
+        }
+
+        // FIXME: I think this is a bug in boogie. IdentifierExpr get cloned twice!
+        // By also overriding this method we prevent IdentifierExpr belonging to symbolics getting cloned
+        public override Expr VisitExpr(Expr node)
+        {
+            if (node is IdentifierExpr && (node as IdentifierExpr).Decl is SymbolicVariable)
+                return (Expr) this.Visit(node); // Skip duplication
+            else
+                return base.VisitExpr(node); // Duplicate as normal
         }
     }
 }
