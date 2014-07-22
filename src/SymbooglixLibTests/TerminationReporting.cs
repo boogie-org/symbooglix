4e70557480c9beac7eece39f7f45bb3b498573ac
Dan Liew <daniel.liew@imperial.ac.uk>
2014-07-22 10:51:10 +0100

    Refactored TerminationReporting unit test to use TerminationCounter TerminationHandler.

--- src/SymbooglixLibTests/TerminationReporting.cs
+++ src/SymbooglixLibTests/TerminationReporting.cs
@@ -4,82 +4,40 @@ using Symbooglix;
 
 namespace SymbooglixLibTests
 {
-    // FIXME: Refactor to use TerminationCounter class
     [TestFixture()]
-    public class TerminationReporting : SymbooglixTest, ITerminationHandler
+    public class TerminationReporting : SymbooglixTest
     {
-        private int failingAssertCount = 0;
-        private int successCount = 0;
-        private int failingEnsuresCount = 0;
-        private int UnsatisfiableAssumeCount = 0;
-        private int UnsatisfiableRequiresCount = 0;
+        public TerminationCounter Counter;
 
-        public void ResetCounters()
+        public void InitAndRun(string program)
         {
-            failingAssertCount = 0;
-            successCount = 0;
-            failingEnsuresCount = 0;
-            UnsatisfiableAssumeCount = 0;
-            UnsatisfiableRequiresCount = 0;
+            Counter = new TerminationCounter();
+            p = loadProgram(program);
+            e = getExecutor(p);
+            e.RegisterTerminationHandler(Counter);
+            e.Run(getMain(p));
         }
 
         [Test()]
         public void FailingAssert()
         {
-            ResetCounters();
-            p = loadProgram("programs/assert_false.bpl");
-            e = getExecutor(p);
-            e.RegisterTerminationHandler(this);
-            e.Run(getMain(p));
-            Assert.AreEqual(failingAssertCount, 1);
+            InitAndRun("programs/assert_false.bpl");
+            Assert.AreEqual(Counter.FailingAsserts, 1);
         }
 
         [Test()]
         public void TerminateWithoutError()
         {
-            ResetCounters();
-            p = loadProgram("programs/assert_true.bpl");
-            e = getExecutor(p);
-            e.RegisterTerminationHandler(this);
-            e.Run(getMain(p));
-            Assert.AreEqual(failingAssertCount, 0);
-            Assert.AreEqual(successCount, 1);
+            InitAndRun("programs/assert_true.bpl");
+            Assert.AreEqual(Counter.FailingAsserts, 0);
+            Assert.AreEqual(Counter.Sucesses, 1);
         }
 
         [Test()]
         public void UnsatAssume()
         {
-            ResetCounters();
-            p = loadProgram("programs/assume_false.bpl");
-            e = getExecutor(p);
-            e.RegisterTerminationHandler(this);
-            e.Run(getMain(p));
-            Assert.AreEqual(UnsatisfiableAssumeCount, 1);
-        }
-
-        public void handleSuccess(ExecutionState s)
-        {
-            ++successCount;
-        }
-
-        public void handleFailingAssert(ExecutionState s)
-        {
-            ++failingAssertCount;
-        }
-
-        public void handleFailingEnsures(ExecutionState s, Microsoft.Boogie.Ensures ensuresStatement)
-        {
-            ++failingEnsuresCount;
-        }
-
-        public void handleUnsatisfiableAssume(ExecutionState s)
-        {
-            ++UnsatisfiableAssumeCount;
-        }
-
-        public void handleUnsatisfiableRequires (ExecutionState s, Microsoft.Boogie.Requires requiresStatement)
-        {
-            ++UnsatisfiableRequiresCount;
+            InitAndRun("programs/assume_false.bpl");
+            Assert.AreEqual(Counter.UnsatisfiableAssumes, 1);
         }
     }
 }
