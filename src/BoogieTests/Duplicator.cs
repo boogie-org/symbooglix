697948be1cbe7f45a06ff026cf8bbec7ec2670e3
Dan Liew <daniel.liew@imperial.ac.uk>
2014-10-04 21:35:46 +0100

    Added unit test for a bug in Boogie's duplicator. It doesn't clone Programs correctly because the TopLevelDeclarations list doesn't get cloned in the Duplicator.

--- src/BoogieTests/Duplicator.cs
+++ src/BoogieTests/Duplicator.cs
@@ -49,6 +49,54 @@ namespace BoogieTests
 
             Assert.IsFalse(Object.ReferenceEquals(A, B));
         }
+
+        [Test()]
+        public void WholeProgram()
+        {
+            var p = new Program();
+            var t = new TypedIdent(Token.NoToken, "foo", Microsoft.Boogie.Type.Bool);
+            var gv = new GlobalVariable(Token.NoToken, t);
+            p.AddTopLevelDeclaration(gv);
+
+            // Now try to clone
+            var p2 = (Program) d.Visit(p);
+
+            // Check global is a copy
+            int counter = 0;
+            GlobalVariable gv2 = null;
+            foreach (var g in p2.TopLevelDeclarations)
+            {
+                ++counter;
+                Assert.IsInstanceOfType(typeof(GlobalVariable), g);
+                gv2 = g as GlobalVariable;
+            }
+            Assert.AreEqual(1, counter);
+            Assert.AreNotSame(gv, gv2);
+
+            // Check Top level declarations list is duplicated properly
+            var t2 = new TypedIdent(Token.NoToken, "bar", Microsoft.Boogie.Type.Bool);
+            var gv3 = new GlobalVariable(Token.NoToken, t2);
+            p2.AddTopLevelDeclaration(gv3);
+
+            counter = 0;
+            foreach (var g in p2.TopLevelDeclarations)
+            {
+                ++counter;
+                Assert.IsInstanceOfType(typeof(GlobalVariable), g);
+            }
+            Assert.AreEqual(2, counter);
+
+            // The original program should still only have one global variable
+            counter = 0;
+            foreach (var g in p.TopLevelDeclarations)
+            {
+                ++counter;
+                Assert.IsInstanceOfType(typeof(GlobalVariable), g);
+            }
+
+            // FIXME: This currently fails in Boogie
+            Assert.AreEqual(1, counter);
+        }
     }
 }
 
