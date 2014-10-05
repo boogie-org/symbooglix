ec2f00bca183846d4475650c321eb77fbcba8e07
Dan Liew <daniel.liew@imperial.ac.uk>
2014-10-05 16:59:59 +0100

    Refactor InstructionStatisticsTests.cs so it is more maintainable.

--- src/SymbooglixLibTests/InstructionStatisticsTests.cs
+++ src/SymbooglixLibTests/InstructionStatisticsTests.cs
@@ -17,7 +17,11 @@ namespace SymbooglixLibTests
             e.UseGotoLookAhead = true;
 
             e.Run(getMain(p));
+            DoTest(4, 1, 3);
+        }
 
+        private void DoTest(int expectedLoopHeadTotalJumps, int expectedLoopHeadJumpsToLoopDone, int expectedLoopHeadJumpsToLoopBody)
+        {
             // Check we executed foo three times
             var foo = p.TopLevelDeclarations.OfType<Implementation>().Where(x => x.Name == "foo").First();
             foreach (var cmd in foo.Blocks[0].Cmds)
@@ -60,9 +64,9 @@ namespace SymbooglixLibTests
 
             // Check the goto of loopHead
             var loopHeadGotoStats = loopHead.TransferCmd.GetInstructionStatistics() as GotoInstructionStatistics;
-            Assert.AreEqual(4, loopHeadGotoStats.TotalJumps);
-            Assert.AreEqual(1, loopHeadGotoStats.GetJumpsTo(loopDone));
-            Assert.AreEqual(3, loopHeadGotoStats.GetJumpsTo(loopBody));
+            Assert.AreEqual(expectedLoopHeadTotalJumps, loopHeadGotoStats.TotalJumps);
+            Assert.AreEqual(expectedLoopHeadJumpsToLoopDone, loopHeadGotoStats.GetJumpsTo(loopDone));
+            Assert.AreEqual(expectedLoopHeadJumpsToLoopBody, loopHeadGotoStats.GetJumpsTo(loopBody));
 
             // Check the goto of loopBody
             var loopBodyGotoStats = loopBody.TransferCmd.GetInstructionStatistics() as GotoInstructionStatistics;
@@ -78,57 +82,7 @@ namespace SymbooglixLibTests
             e.UseGotoLookAhead = false;
 
             e.Run(getMain(p));
-
-            // Check we executed foo three times
-            var foo = p.TopLevelDeclarations.OfType<Implementation>().Where(x => x.Name == "foo").First();
-            foreach (var cmd in foo.Blocks[0].Cmds)
-                Assert.AreEqual(3, cmd.GetInstructionStatistics().Covered);
-
-            Assert.AreEqual(3, (foo.Blocks[0].TransferCmd as ReturnCmd).GetInstructionStatistics().Covered);
-
-            // Check foo's requires and ensures were covered
-            foreach (var requires in foo.Proc.Requires)
-                Assert.AreEqual(3, requires.GetInstructionStatistics().Covered);
-
-            foreach (var ensures in foo.Proc.Ensures)
-                Assert.AreEqual(3, ensures.GetInstructionStatistics().Covered);
-
-            // Check execution of main
-            var main = p.TopLevelDeclarations.OfType<Implementation>().Where(x => x.Name == "main").First();
-
-            var entryBlock = main.Blocks[0];
-            Assert.AreEqual("anon0", entryBlock.Label);
-            CheckBlockCount(entryBlock, 1);
-
-            var loopHead = main.Blocks[1];
-            Assert.AreEqual("anon2_LoopHead", loopHead.Label);
-            CheckBlockCount(loopHead, 4);
-
-            var loopBody = main.Blocks[2];
-            Assert.AreEqual("anon2_LoopBody", loopBody.Label);
-
-            // The assume gets visited one extra time
-            CheckBlockStartingWithAssumeCount(loopBody, 4, 3);
-
-            var loopDone = main.Blocks[3];
-            Assert.AreEqual("anon2_LoopDone", loopDone.Label);
-            CheckBlockStartingWithAssumeCount(loopDone, 4, 1);
-
-            // Check the goto of entry block
-            var gotoStats = entryBlock.TransferCmd.GetInstructionStatistics() as GotoInstructionStatistics;
-            Assert.AreEqual(1, gotoStats.TotalJumps);
-            Assert.AreEqual(1, gotoStats.GetJumpsTo(loopHead));
-
-            // Check the goto of loopHead
-            var loopHeadGotoStats = loopHead.TransferCmd.GetInstructionStatistics() as GotoInstructionStatistics;
-            Assert.AreEqual(8, loopHeadGotoStats.TotalJumps);
-            Assert.AreEqual(4, loopHeadGotoStats.GetJumpsTo(loopDone));
-            Assert.AreEqual(4, loopHeadGotoStats.GetJumpsTo(loopBody));
-
-            // Check the goto of loopBody
-            var loopBodyGotoStats = loopBody.TransferCmd.GetInstructionStatistics() as GotoInstructionStatistics;
-            Assert.AreEqual(3, loopBodyGotoStats.TotalJumps);
-            Assert.AreEqual(3, loopBodyGotoStats.GetJumpsTo(loopHead));
+            DoTest(8, 4, 4);
         }
 
         private void CheckBlockCount(Block bb, int count)
