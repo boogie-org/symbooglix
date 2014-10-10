de9df9f7cf7643777d68a18a9fe2925456d94c79
Dan Liew <daniel.liew@imperial.ac.uk>
2014-10-10 17:55:22 +0100

    Move DFSStateScheduler into its own file.

--- src/Symbooglix/Executor/StateSchedulers/StateScheduler.cs
+++ src/Symbooglix/Executor/StateSchedulers/StateScheduler.cs
@@ -13,56 +13,5 @@ namespace Symbooglix
         void RemoveAll(Predicate<ExecutionState> p);
         int GetNumberOfStates();
     }
-
-    public class DFSStateScheduler : IStateScheduler
-    {
-        private List<ExecutionState> States;
-        public DFSStateScheduler() 
-        { 
-            States = new List<ExecutionState>();
-        }
-
-        public ExecutionState GetNextState() 
-        { 
-            if (States.Count == 0)
-                return null;
-
-            return States[0];
-        }
-
-        public void UpdateStates(List<ExecutionState> toAdd, List<ExecutionState> toRemove)
-        {
-            foreach(ExecutionState e in toRemove)
-            {
-                Debug.Assert(States.Contains(e));
-                States.Remove(e);
-            }
-
-            // Add to end of List
-            foreach(ExecutionState e in toAdd)
-            {
-                States.Add(e);
-            }
-        }
-
-        public int GetNumberOfStates() { return States.Count;}
-
-        public void AddState (ExecutionState toAdd)
-        {
-            States.Add(toAdd);
-        }
-
-        public void RemoveState(ExecutionState toRemove)
-        {
-            Debug.Assert(States.Contains(toRemove), "Cannot remove state not stored in the state scheduler");
-            States.Remove(toRemove);
-        }
-
-        public void RemoveAll(Predicate<ExecutionState> p)
-        {
-            States.RemoveAll(p);
-        }
-
-    }
 }
 
