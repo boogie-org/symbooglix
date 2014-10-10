fdfa16da32699730737ace2919a8794684dbd6d7
Dan Liew <daniel.liew@imperial.ac.uk>
2014-10-10 18:00:04 +0100

    Copy broken DFSStateScheduler code into another file and call it UntilTerminationBFSStateScheduler. I want to keep this implementation around so it can be tested as an alternative scheduler.

---
+++ src/Symbooglix/Executor/StateSchedulers/UntilTerminationBFSStateScheduler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Symbooglix
+{
+    /// <summary>
+    /// This state scheduler visits states in BFS order but lets
+    /// an ExecutionState terminate before it will consider the next
+    /// state to visit.
+    /// </summary>
+    public class UntilTerminationBFSStateScheduler : IStateScheduler
+    {
+        private List<ExecutionState> States;
+        public UntilTerminationBFSStateScheduler() 
+        { 
+            States = new List<ExecutionState>();
+        }
+
+        public ExecutionState GetNextState() 
+        { 
+            if (States.Count == 0)
+                return null;
+
+            return States[0];
+        }
+
+        public int GetNumberOfStates() { return States.Count;}
+
+        public void AddState (ExecutionState toAdd)
+        {
+            States.Add(toAdd);
+        }
+
+        public void RemoveState(ExecutionState toRemove)
+        {
+            Debug.Assert(States.Contains(toRemove), "Cannot remove state not stored in the state scheduler");
+            States.Remove(toRemove);
+        }
+
+        public void RemoveAll(Predicate<ExecutionState> p)
+        {
+            States.RemoveAll(p);
+        }
+    }
+}
+
