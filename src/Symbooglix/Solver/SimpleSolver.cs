e03291d536e2ccbed58337617b9e4181b2b5ff91
Dan Liew <daniel.liew@imperial.ac.uk>
2015-01-05 21:38:29 +0000

    Refactor calls in SimpleSolver to the implementation. What I wanted to do was implement a timeout at that level because really the solver timeout should include stuff in the SolverImpl layers.
    I had an implementation that used System.Threading.Task but the problem
    with Tasks is you can't abort them and we also need a way of making a
    new SolverImpl because we have no idea what state the SolverImpl is left
    in. So for now I'll things as they are.

--- src/Symbooglix/Solver/SimpleSolver.cs
+++ src/Symbooglix/Solver/SimpleSolver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Symbooglix
 {
@@ -10,6 +11,7 @@ namespace Symbooglix
         {
             protected Stopwatch Timer;
             private SolverStats InternalStatistics;
+            private int Timeout = -1; // No timeout by default
             public ISolverImpl SolverImpl
             {
                 get;
@@ -31,13 +33,28 @@ namespace Symbooglix
             public void SetTimeout(int seconds)
             {
                 SolverImpl.SetTimeout(seconds);
+                this.Timeout = seconds * 1000;
+            }
+
+            private Tuple<Result, IAssignment> CallImplementation(Microsoft.Boogie.Expr queryExpr, bool getAssignment)
+            {
+                // FIXME: We need to enforce the timeout here but doing so isn't possible with the current design.
+                // If we implement a timeout then we need away to create a new solverImplementation when the timeout hits
+                // because we'll need a way to throw away the old solver (we have no idea what states its in).
+                /// We can't do that right now, we need to introduce
+                // an ISolverImplFactory that we take so we can create new solver implementations whenever it's necessary.
+
+                //Console.WriteLine("Starting solver");
+                var result = SolverImpl.ComputeSatisfiability(queryExpr, getAssignment);
+                //Console.WriteLine("Finished solver");
+                return result;
             }
 
             public Result IsQuerySat(Microsoft.Boogie.Expr query, out IAssignment assignment)
             {
                 Timer.Start();
 
-                var result = SolverImpl.ComputeSatisfiability(query, true);
+                var result = CallImplementation(query, true);
                 assignment = result.Item2;
                 Debug.Assert(assignment != null, "Assignment object cannot be null");
 
@@ -50,7 +67,7 @@ namespace Symbooglix
             {
                 Timer.Start();
 
-                var result = SolverImpl.ComputeSatisfiability(query, false);
+                var result = CallImplementation(query, false);
 
                 Timer.Stop();
                 UpdateStatistics(result);
@@ -61,7 +78,7 @@ namespace Symbooglix
             {
                 Timer.Start();
 
-                var result = SolverImpl.ComputeSatisfiability(Microsoft.Boogie.Expr.Not(query), true);
+                var result = CallImplementation(Microsoft.Boogie.Expr.Not(query), true);
                 assignment = result.Item2;
                 Debug.Assert(assignment != null, "Assignment object cannot be null");
 
@@ -74,7 +91,7 @@ namespace Symbooglix
             {
                 Timer.Start();
 
-                var result = SolverImpl.ComputeSatisfiability(Microsoft.Boogie.Expr.Not(query), false);
+                var result = CallImplementation(Microsoft.Boogie.Expr.Not(query), false);
 
                 Timer.Stop();
                 UpdateStatistics(result);
