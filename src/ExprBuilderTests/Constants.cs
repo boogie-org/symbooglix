8046bb9eca35212e86a73e784bf81b2d723aa951
Dan Liew <daniel.liew@imperial.ac.uk>
2015-01-07 00:31:45 +0000

    Refactor type check assertions in unit test and test Integer creation too.

--- src/ExprBuilderTests/Constants.cs
+++ src/ExprBuilderTests/Constants.cs
@@ -1,29 +1,32 @@
 ﻿using NUnit.Framework;
 using System;
 using Symbooglix;
+using Microsoft.Basetypes;
 
 namespace ExprBuilderTests
 {
     [TestFixture()]
     public class Constants
     {
+        private void CheckType(Microsoft.Boogie.LiteralExpr e, Predicate<Microsoft.Boogie.Type> p)
+        {
+            Assert.IsNotNull(e.ShallowType);
+            Assert.IsTrue(p(e.ShallowType));
+            Assert.IsNotNull(e.Type);
+            Assert.AreEqual(e.ShallowType, e.Type);
+        }
+
         [Test()]
         public void True()
         {
             var builder = new SimpleExprBuilder();
             var constant = builder.True;
             Assert.AreEqual("true", constant.ToString());
-
-            Assert.IsNotNull(constant.Type);
-            Assert.AreEqual(constant.ShallowType, constant.Type);
-            Assert.IsTrue(constant.Type.IsBool);
+            CheckType(constant, t => t.IsBool);
 
             var constant2 = builder.ConstantBool(true);
             Assert.AreEqual("true", constant2.ToString());
-
-            Assert.IsNotNull(constant2.Type);
-            Assert.AreEqual(constant2.ShallowType, constant.Type);
-            Assert.IsTrue(constant2.Type.IsBool);
+            CheckType(constant2, t => t.IsBool);
         }
 
         [Test()]
@@ -32,17 +35,31 @@ namespace ExprBuilderTests
             var builder = new SimpleExprBuilder();
             var constant = builder.False;
             Assert.AreEqual("false", constant.ToString());
-
-            Assert.IsNotNull(constant.Type);
-            Assert.AreEqual(constant.ShallowType, constant.Type);
-            Assert.IsTrue(constant.Type.IsBool);
+            CheckType(constant, t => t.IsBool);
 
             var constant2 = builder.ConstantBool(false);
             Assert.AreEqual("false", constant2.ToString());
+            CheckType(constant, t => t.IsBool);
+        }
 
-            Assert.IsNotNull(constant2.Type);
-            Assert.AreEqual(constant2.ShallowType, constant.Type);
-            Assert.IsTrue(constant2.Type.IsBool);
+        [Test()]
+        public void PositiveInteger()
+        {
+            var builder = new SimpleExprBuilder();
+            var constant = builder.ConstantInt(5);
+            Assert.AreEqual("5", constant.ToString());
+            Assert.AreEqual(5, constant.asBigNum.ToInt);
+            CheckType(constant, t => t.IsInt);
+        }
+
+        [Test()]
+        public void NegativeInteger()
+        {
+            var builder = new SimpleExprBuilder();
+            var constant = builder.ConstantInt(-5);
+            Assert.AreEqual("-5", constant.ToString());
+            Assert.AreEqual(-5, constant.asBigNum.ToInt);
+            CheckType(constant, t => t.IsInt);
         }
     }
 }
