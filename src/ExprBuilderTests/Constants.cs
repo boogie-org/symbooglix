52550a371640ae1cd89bc8ecc787705d6bd795f6
Dan Liew <daniel.liew@imperial.ac.uk>
2015-01-07 00:18:12 +0000

    Add asserts for types in Constants unit tests for SimpleExprBuilder

--- src/ExprBuilderTests/Constants.cs
+++ src/ExprBuilderTests/Constants.cs
@@ -14,8 +14,16 @@ namespace ExprBuilderTests
             var constant = builder.True;
             Assert.AreEqual("true", constant.ToString());
 
+            Assert.IsNotNull(constant.Type);
+            Assert.AreEqual(constant.ShallowType, constant.Type);
+            Assert.IsTrue(constant.Type.IsBool);
+
             var constant2 = builder.ConstantBool(true);
             Assert.AreEqual("true", constant2.ToString());
+
+            Assert.IsNotNull(constant2.Type);
+            Assert.AreEqual(constant2.ShallowType, constant.Type);
+            Assert.IsTrue(constant2.Type.IsBool);
         }
 
         [Test()]
@@ -25,8 +33,16 @@ namespace ExprBuilderTests
             var constant = builder.False;
             Assert.AreEqual("false", constant.ToString());
 
+            Assert.IsNotNull(constant.Type);
+            Assert.AreEqual(constant.ShallowType, constant.Type);
+            Assert.IsTrue(constant.Type.IsBool);
+
             var constant2 = builder.ConstantBool(false);
             Assert.AreEqual("false", constant2.ToString());
+
+            Assert.IsNotNull(constant2.Type);
+            Assert.AreEqual(constant2.ShallowType, constant.Type);
+            Assert.IsTrue(constant2.Type.IsBool);
         }
     }
 }
