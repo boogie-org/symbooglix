df5bf741b163717721c330e9827014c0bd27efd4
Dan Liew <daniel.liew@imperial.ac.uk>
2015-01-26 20:51:14 +0000

    Implement basic constant folding of Add operator in ConstantFoldingExprBuilder. This fixes several tests that depend on this behaviour.

--- src/Symbooglix/Expr/ConstantFoldingExprBuilder.cs
+++ src/Symbooglix/Expr/ConstantFoldingExprBuilder.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Microsoft.Boogie;
+using Microsoft.Basetypes;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Symbooglix
 {
@@ -9,6 +13,28 @@ namespace Symbooglix
         }
 
         // TODO Overload methods that we can constant fold
+
+        public override Expr Add(Expr lhs, Expr rhs)
+        {
+            if (lhs is LiteralExpr && rhs is LiteralExpr)
+            {
+                var literalLhs = lhs as LiteralExpr;
+                var literalRhs = rhs as LiteralExpr;
+                if (literalLhs.isBigNum && literalRhs.isBigNum)
+                {
+                    // Int
+                    var result = literalLhs.asBigNum + literalRhs.asBigNum;
+                    return UB.ConstantInt(result.ToBigInteger);
+                }
+                else if (literalLhs.isBigDec && literalRhs.isBigDec)
+                {
+                    // Real
+                    return UB.ConstantReal(literalLhs.asBigDec + literalRhs.asBigDec);
+                }
+            }
+
+            return UB.Add(lhs, rhs);
+        }
     }
 }
 
