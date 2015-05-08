6ac085f82a3c9b97eff19e6412229afdcc1aa61d
Dan Liew <daniel.liew@imperial.ac.uk>
2015-05-08 15:42:09 +0100

    Implement ConstantCachingBuilder with unit tests. The tests for reals fails currently because there is a bug in Boogie where comparison of BigDec is incorrectly implemented.

---
+++ src/ExprBuilderTests/ConstantCacheBuilderTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using NUnit.Framework;
+using Symbooglix;
+using Microsoft.Boogie;
+using System.Numerics;
+using Microsoft.Basetypes;
+
+namespace ExprBuilderTests
+{
+    [TestFixture()]
+    public class ConstantCacheBuilderTests : SimpleExprBuilderTestBase
+    {
+        [Test()]
+        public void testBoolean()
+        {
+            var ccB = new ConstantCachingBuilder(this.GetSimpleBuilder());
+
+            var trueExpr = ccB.True;
+            Assert.IsTrue(trueExpr.isBool && trueExpr.asBool);
+            var falseExpr = ccB.False;
+            Assert.IsTrue(falseExpr.isBool && !falseExpr.asBool);
+
+            Assert.AreSame(trueExpr, ccB.True);
+            Assert.AreSame(falseExpr, ccB.False);
+
+            // Test other interface
+            Assert.AreSame(trueExpr, ccB.ConstantBool(true));
+            Assert.AreSame(falseExpr, ccB.ConstantBool(false));
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void testBv(bool useIntInterface)
+        {
+            var ccB = new ConstantCachingBuilder(this.GetSimpleBuilder());
+
+            for (int bitWidth = 1; bitWidth < 16; ++bitWidth)
+            {
+                for (int value = 0; value < (1 << bitWidth); ++value)
+                {
+                    var bvExpr = useIntInterface ? ccB.ConstantBV(value, bitWidth) : ccB.ConstantBV(new BigInteger(value), bitWidth);
+                    var bvExpr2 = useIntInterface ? ccB.ConstantBV(value, bitWidth) : ccB.ConstantBV(new BigInteger(value), bitWidth);
+                    Assert.AreSame(bvExpr, bvExpr2);
+                }
+            }
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void testInt(bool useIntInterface)
+        {
+            var ccB = new ConstantCachingBuilder(this.GetSimpleBuilder());
+
+            for (int value = -1000; value < 1000; ++value)
+            {
+                var intExpr = useIntInterface ? ccB.ConstantInt(value) : ccB.ConstantInt(new BigInteger(value));
+                var intExpr2 = useIntInterface ? ccB.ConstantInt(value) : ccB.ConstantInt(new BigInteger(value));
+                Assert.AreSame(intExpr, intExpr2);
+            }
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void testReal(bool useStringInterface)
+        {
+            var ccB = new ConstantCachingBuilder(this.GetSimpleBuilder());
+
+            for (BigDec value = BigDec.FromInt(-20); value <= BigDec.FromInt(20); value += BigDec.FromString("0.1"))
+            {
+                var asString = value.ToString();
+                var realExpr = useStringInterface ? ccB.ConstantReal(asString) : ccB.ConstantReal(value);
+                var realExpr2 = useStringInterface ? ccB.ConstantReal(asString) : ccB.ConstantReal(value);
+                Assert.AreSame(realExpr, realExpr2);
+            }
+        }
+
+    }
+}
+
---
+++ src/Symbooglix/ConstantCachingBuilder.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.Boogie;
+using System.Numerics;
+using Microsoft.Basetypes;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Symbooglix
+{
+    public class ConstantCachingBuilder : DecoratorExprBuilder
+    {
+        private LiteralExpr CachedTrueExpr;
+        private LiteralExpr CachedFalseExpr;
+        private Dictionary<BigInteger, LiteralExpr> IntegerCache; // FIXME: Should probably use concurrent dictionary for thread safety
+        private Dictionary<BigDec, LiteralExpr> RealCache; // FIXME: Should probably use concurrent dictionary for thread safety
+        private Dictionary<int, Dictionary<BigInteger,LiteralExpr>> BitVectorCache; // FIXME: Should probably use concurrent dictionary for thread safety
+        public ConstantCachingBuilder(IExprBuilder underlyingBuilder) : base(underlyingBuilder)
+        {
+            // It's not safe to cache expressions if the underlying builder does not give immutable LiteralExprs
+            if (!UB.Immutable)
+                throw new ArgumentException("Underlying builder must build immutable expressions");
+
+            // Setup Caches
+            this.CachedTrueExpr = UB.True;
+            Debug.Assert(this.CachedTrueExpr.isBool && this.CachedTrueExpr.asBool);
+            this.CachedFalseExpr = UB.False;
+            Debug.Assert(this.CachedFalseExpr.isBool && !this.CachedFalseExpr.asBool);
+            this.IntegerCache = new Dictionary<BigInteger, LiteralExpr>();
+            this.RealCache = new Dictionary<BigDec, LiteralExpr>();
+            this.BitVectorCache = new Dictionary<int, Dictionary<BigInteger, LiteralExpr>>();
+        }
+
+        public override LiteralExpr True
+        {
+            get
+            {
+                return this.ConstantBool(true);
+            }
+        }
+
+        public override LiteralExpr False
+        {
+            get
+            {
+                return this.ConstantBool(false);
+            }
+        }
+
+        public override LiteralExpr ConstantBool(bool value)
+        {
+            if (value)
+                return CachedTrueExpr;
+            else
+                return CachedFalseExpr;
+        }
+
+
+        public override LiteralExpr ConstantInt(int value)
+        {
+            return this.ConstantInt(new BigInteger(value));
+        }
+
+        public override LiteralExpr ConstantInt(BigInteger decimalValue)
+        {
+            try
+            {
+                var cachedInt = IntegerCache[decimalValue];
+                Debug.Assert(cachedInt.isBigNum);
+                return cachedInt;
+            }
+            catch (KeyNotFoundException)
+            {
+                var newInt = UB.ConstantInt(decimalValue);
+                Debug.Assert(newInt.isBigNum);
+                IntegerCache.Add(decimalValue, newInt);
+                return newInt;
+            }
+        }
+
+        public override LiteralExpr ConstantReal(string value)
+        {
+            return base.ConstantReal(BigDec.FromString(value));
+        }
+
+        public override LiteralExpr ConstantReal(BigDec value)
+        {
+            try
+            {
+                var cachedReal = RealCache[value];
+                Debug.Assert(cachedReal.isBigDec);
+                return cachedReal;
+            }
+            catch (KeyNotFoundException)
+            {
+                var newReal = UB.ConstantReal(value);
+                Debug.Assert(newReal.isBigDec);
+                RealCache.Add(value, newReal);
+                return newReal;
+            }
+        }
+
+
+        public override LiteralExpr ConstantBV(int decimalValue, int bitWidth)
+        {
+            return this.ConstantBV(new BigInteger(decimalValue), bitWidth);
+        }
+
+        public override LiteralExpr ConstantBV(BigInteger decimalValue, int bitWidth)
+        {
+            // Get the right dictionary for the bitwidth
+            Dictionary<BigInteger, LiteralExpr> mapping = null;
+            try
+            {
+                mapping = BitVectorCache[bitWidth];
+            }
+            catch (KeyNotFoundException)
+            {
+                mapping = new Dictionary<BigInteger, LiteralExpr>();
+                BitVectorCache.Add(bitWidth, mapping);
+            }
+
+            try
+            {
+                var constant = mapping[decimalValue];
+                Debug.Assert(constant.isBvConst);
+                return constant;
+            }
+            catch (KeyNotFoundException)
+            {
+                var newBv = UB.ConstantBV(decimalValue, bitWidth);
+                Debug.Assert(newBv.isBvConst);
+                mapping.Add(decimalValue, newBv);
+                return newBv;
+            }
+        }
+
+
+    }
+}
+
