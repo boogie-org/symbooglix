447939ac783c88c4e5609ca4d5d2e4847442e00c
Liam Machado <liam.machado@yahoo.com>
2018-02-02 12:33:29 -0700

    Fixed unreachable nodes in generated CFGs not being removed
    Unreachable nodes being present in generated CFGs was resulting in an
    ArgumentNullException being thrown in Boogie code.

--- src/Symbooglix/Analysis/LoopInfo.cs
+++ src/Symbooglix/Analysis/LoopInfo.cs
@@ -54,6 +54,7 @@ namespace Symbooglix
         {
             // Compute the basic blocks that escape loops
             foreach (var impl in Prog.Implementations) {
+                impl.PruneUnreachableBlocks();
                 var CFG = Program.GraphFromImpl(impl);
                 CFG.ComputeLoops();
                 foreach (var Header in CFG.Headers) {
